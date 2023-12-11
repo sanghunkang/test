712c1b7fd2db034f6bcfd9fbdc145f05ff77d404
kimkmk <kkm2007@gmail.com>
2023-12-11 23:31:05 +0900

    경민 디자인
    첫화면 디자인
    풋터 디자인
    pc에서 봐도 폰 비율 비슷하게 보이게 설정

--- src/app/sampleData.ts
+++ src/app/sampleData.ts
@@ -112,7 +112,7 @@ export const recipes: Recipe[] = [
   },
   {
     id: 2,
-    name: '{{여기에 텍스트 입력}}',
+    name: '{{여기에 텍스트 경민 테스트 입력}}',
     style: '{{여기에 텍스트 입력}}',
     level: '중',
     time: '15분',
--- src/pages/RecipeListPage.tsx
+++ src/pages/RecipeListPage.tsx
@@ -4,13 +4,20 @@ import {
   Grid,
   Paper,
 } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import TagOutlinedIcon from '@mui/icons-material/TagOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
 import { CommonHeader } from '../components/CommonHeader';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { getRecipes } from '../app/sampleData';
 import { Recipe } from '../app/types';
 import {
-  RECIPE_PAGE_PATH
+  RECIPE_PAGE_PATH,
+  HOME_PAGE_PATH,
+  STYLES_PAGE_PATH,
+  REPORT_PAGE_PATH,
 } from '../App';
 // import { useGetNewsTrendsQuery } from '../app/newsApi';
 
@@ -50,11 +57,69 @@ function RecipeListPageBody() {
   );
 }
 
+function HomePageFooter() {
+  const navigate = useNavigate();
+
+  return (
+    <Grid container className='app-footer'>
+      <Grid item xs={3}>
+        <div>
+
+          <HomeIcon
+            sx={{ fontSize: '32px' }}
+            onClick={(e) => { navigate(HOME_PAGE_PATH) }}
+          />
+        </div>
+        <div>
+          홈
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+
+          <RestaurantOutlinedIcon
+            sx={{ fontSize: '32px' }}
+            onClick={(e) => { navigate(RECIPE_PAGE_PATH) }}
+          />
+        </div>
+        <div>
+          재료별
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+
+          <TagOutlinedIcon
+            sx={{ fontSize: '32px' }}
+            onClick={(e) => { navigate(STYLES_PAGE_PATH) }}
+          />
+        </div>
+        <div>
+          테마별
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+          <PersonOutlineOutlinedIcon
+            sx={{ fontSize: '32px' }}
+            onClick={(e) => { navigate(REPORT_PAGE_PATH) }}
+          />
+        </div>
+        <div>
+          절약리포트
+        </div>
+
+      </Grid>
+    </Grid>
+  );
+};
+
 export default function RecipeListPage() {
   return (
     <div>
       <CommonHeader />
       <RecipeListPageBody />
+      <HomePageFooter />
     </div>
 
   );
